a5e61b65c0738b01090db62fdd5392c0ccc7051b
realDonner <3070460093@qq.com>
2020-02-04 15:35:36 +0800

    333

--- src/home0204/yangchunlei/Test.java
+++ src/home0204/yangchunlei/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         CarBuilder carBuilder = new CarRealBuilder("奔驰车","奔驰发动机","奔驰车轮","奔驰车身");
         CarBuilder carBuilder2 = new CarRealBuilder("宝马车","宝马发动机","宝马车轮","宝马车身","区别标志");
+        System.out.println("111");
     }
 }
