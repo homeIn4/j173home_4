7913c3d52d60b58a359b6a70f4d2ce2cf772cb80
realDonner <3070460093@qq.com>
2020-02-05 16:22:53 +0800

    0205作业上传

---
+++ src/home0205/yangchunlei/TestMap.java
@@ -0,0 +1,50 @@
+package home0205.yangchunlei;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 模拟缓存
+ */
+public class TestMap {
+    public static void main(String[] args) {
+        // 数据库
+        List<Map<String,User>> mapsDB = new ArrayList<>();
+        // 缓存
+        List<Map<String,User>> mapsReady = new ArrayList<>();
+        boolean tag = false;
+        for (int i = 0; i < 20; i ++){
+            User user = new User("用户" + i,i);
+            Map<String,User> map = new HashMap<>();
+            map.put(i + "",user);
+            if (i <10){
+                mapsReady.add(map);
+            }
+            mapsDB.add(map);
+        }
+        System.out.println("缓存中数据条数:" + mapsReady.size());
+        System.out.println("数据库中数据条数:" + mapsDB.size());
+        // 查找年龄为X的用户
+        String age = "15";
+        for (Map map:mapsReady){
+            if (map.get(age) != null){
+                tag = true;
+                System.out.println("在缓存中找到了年龄为" + age + "的用户");
+            }
+        }
+        if (!tag){
+            System.out.println("从缓存中没找到年龄为" + age + "的用户");
+            System.out.println("准备从数据库中查找");
+            for (Map map:mapsDB){
+                if (map.get(age) != null){
+                    System.out.println("在数据库中找到了年龄为" + age + "的用户");
+                    mapsReady.add(map);
+                }
+            }
+        }
+        System.out.println("缓存中数据条数:" + mapsReady.size());
+        System.out.println("数据库中数据条数:" + mapsDB.size());
+    }
+}
--- src/home0205/yangchunlei/User.java
+++ src/home0205/yangchunlei/User.java
@@ -1,4 +1,33 @@
 package home0205.yangchunlei;
 
+/**
+ *用户
+ */
 public class User {
+    private String userName;
+    private int age;
+
+    public User() {
+    }
+
+    public User(String userName, int age) {
+        this.userName = userName;
+        this.age = age;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
